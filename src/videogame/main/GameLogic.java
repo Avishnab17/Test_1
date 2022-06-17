55ea6c0e4a90a57e050848f29abd60a3b0535f8f
yashi <yashinayurvi@gmail.com>
2022-06-17 22:03:17 +0400

    added randomBattle

--- src/videogame/main/GameLogic.java
+++ src/videogame/main/GameLogic.java
@@ -2,4 +2,12 @@ package videogame.main;
 
 public class GameLogic {
 
+public static void randomBattle(){
+    clearConsole();
+    printHeading("You encountered an evil minded creature. You'll have to fight it!");
+    anythingToContine();
+    battle(new Enemy(enemies[(int)(Math.random()*enemies.length)],player.xp));
+}
+
+
 }
